b1d902537371c47837129cc9ed852fecde8a180a
Ratchaporn <hrkfan321@gmail.com>
2023-08-14 16:53:25 +0700

    Add Student list, Edit db.json
    Create student component, create information service, editing db.json

---
+++ src/services/InformationService.ts
@@ -0,0 +1,17 @@
+import axios from 'axios'
+import type { AxiosInstance, AxiosResponse } from 'axios'
+import type { Information } from '@/info'
+const apiClient: AxiosInstance = axios.create({
+    baseURL: "http://localhost:3004",
+    withCredentials: false,
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+    }
+})
+
+export default {
+    getStudent(): Promise<AxiosResponse<Information[]>> {
+        return apiClient.get<Information[]>('/students')
+    }
+}
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,29 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import StudentCard from '@/components/StudentCard.vue';
+import type { Information } from '@/info';
+import InformationService from '@/services/InformationService'
+import type { Ref } from 'vue'
+import { ref } from 'vue'
+
+const students: Ref<Array<Information>> = ref([])
+  InformationService.getStudent().then((response) => {
+    students.value = response.data
+  })
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="student">
+      <StudentCard v-for="student in students" :key="student.id" :student="student"></StudentCard>
+    </div>
+
   </main>
 </template>
+<style>
+.student {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
