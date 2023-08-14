16707a94a432ed2dd4eae71f7c811159d8ab7572
0ctopusHead <yanwarut_s@cmu.ac.th>
2023-08-14 16:56:20 +0700

    Squashed commit of the following:
    commit b1d902537371c47837129cc9ed852fecde8a180a
    Author: Ratchaporn <hrkfan321@gmail.com>
    Date:   Mon Aug 14 16:53:25 2023 +0700
    
        Add Student list, Edit db.json
    
        Create student component, create information service, editing db.json
    
    commit 011c1bbbfbd851dc02dfc81a343369124fe86023
    Author: tommyyeebai <115710017+tommyyeebai@users.noreply.github.com>
    Date:   Mon Aug 14 16:08:43 2023 +0700
    
        rewrite data
    
    commit b7e66a8830d793dd2098effddbb84cb3136131dc
    Author: tommyyeebai <115710017+tommyyeebai@users.noreply.github.com>
    Date:   Mon Aug 14 15:57:27 2023 +0700
    
        update data
    
    commit 78b10d19f1794fe3812e60205a608ae6446dffdc
    Author: tommyyeebai <115710017+tommyyeebai@users.noreply.github.com>
    Date:   Mon Aug 14 13:49:11 2023 +0700
    
        update photo and data
    
    commit ea6e638c196e109102774147749fabed37a945aa
    Author: tommyyeebai <115710017+tommyyeebai@users.noreply.github.com>
    Date:   Mon Aug 14 13:27:41 2023 +0700
    
        add more data to db.json
    
    commit b9e370f8302b0705a05cc2bda1a6b791d39d4563
    Author: tommyyeebai <115710017+tommyyeebai@users.noreply.github.com>
    Date:   Mon Aug 14 13:20:34 2023 +0700
    
        add images

---
+++ src/components/StudentCard.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import type { Information } from '../info'
+import type { PropType } from 'vue'
+const props = defineProps({
+  student: {
+    type: Object as PropType<Information>,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="student-card">
+    
+    <span> Student ID: {{ student.studentID }}</span>
+    <p> Name: {{ student.name }}</p>
+    <p> Surname: {{ student.surname }}</p>
+  </div>
+</template>
+
+<style scoped>
+.student-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: solid 1px black;
+  margin-bottom: 18px;
+}
+
+.student-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+
+</style>
