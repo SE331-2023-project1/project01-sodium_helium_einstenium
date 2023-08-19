acea4ef3014713651fe6e4588bd1d839d0981daa
Ratchaporn <hrkfan321@gmail.com>
2023-08-19 14:09:34 +0700

    Add router push
    Add router push once add data of student finish user will be taken to student details

--- src/views/student/StudentAddView.vue
+++ src/views/student/StudentAddView.vue
@@ -3,7 +3,9 @@ import type { AdvisorInfo, StudentInfo } from '@/info'
 import type { PropType } from 'vue'
 import StudentAddDataForm from '@/components/StudentAddDataForm.vue'
 import { useStudentStore } from '@/stores/student';
-defineProps({
+import { useRouter } from 'vue-router';
+const router = useRouter()
+const props = defineProps({
     student: {
         type: Object as PropType<StudentInfo>,
         require: true
@@ -21,6 +23,7 @@ function addStudentDataBtn(studentData: StudentInfo){
    console.log( studentData.id)
    store.addStudentData(studentData)
    console.log('After Add Btn ' + studentData.id)
+   router.push({name: 'student-detail', params: {id: props.student?.id}});
 }
 </script>
 <template>
