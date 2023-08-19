07e22dbae6af0baea53ee7d04767611797d8d9cd
Ratchaporn <hrkfan321@gmail.com>
2023-08-19 14:17:11 +0700

    Add router push of advisor
    Once another advisor is created or added the user will be taken to the list of all advisors (including the one created latest)

--- src/views/CreateAdvisorView.vue
+++ src/views/CreateAdvisorView.vue
@@ -3,8 +3,9 @@ import AdvisorCreateForm from '@/components/AdvisorCreateForm.vue'
 import type { AdvisorInfo } from '@/info';
 import { useAdvisorStore } from '@/stores/advisor';
 import type { PropType } from 'vue'
-
-defineProps({
+import { useRouter } from 'vue-router';
+const router = useRouter();
+const props = defineProps({
     advisor: {
         type: Object as PropType<AdvisorInfo>,
         require: true
@@ -14,6 +15,7 @@ const store = useAdvisorStore();
 function createAdvisor(advisor: AdvisorInfo){
     console.log("Before add advisor" + advisor.name)
     store.addAdvisor(advisor)
+    router.push({ name: 'advisor-list', params: { id: props.advisor?.id}})
 }
 </script>
 <template>
