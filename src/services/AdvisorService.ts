02fa4aff977454915daabdb7e131081a831cbbe4
0ctopusHead <yanwarut_s@cmu.ac.th>
2023-08-23 22:24:29 +0700

    add .env files

--- src/services/AdvisorService.ts
+++ src/services/AdvisorService.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import type { AxiosInstance, AxiosResponse } from 'axios'
 import type { AdvisorInfo } from '@/info'
 const apiClient: AxiosInstance = axios.create({
-    baseURL: "http://localhost:3004",
+    baseURL: import.meta.env.VITE_BACKEND_URL,
     withCredentials: false,
     headers: {
         Accept: 'application/json',
